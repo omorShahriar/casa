9c7a6be1f01dd8b28c30dd9fa1aa834a2b16d3cb
omorShahriar <omor.shahriar2000@gmail.com>
2022-06-24 17:09:49 +0600

    main link

--- components/Nav.js
+++ components/Nav.js
@@ -1,30 +1,74 @@
-import Image from "next/image"
-import Link from "next/link"
-import Container from "./Container"
-import Logo from "/public/logo.png"
+import Image from "next/image";
+import Link from "next/link";
+import Container from "./Container";
+import Logo from "/public/logo.png";
 const Nav = () => {
   return (
-      <header className=" font-poppins ">
-          <Container><div className='  flex justify-between items-center '>
+    <header className=" font-poppins ">
+      <Container>
+        <div className="  flex justify-between items-center ">
+          <Link href="/">
+            <a>
+              {" "}
               <div className=" w-40 h-40 ">
-                  <Image alt="logo" src={Logo} layout="responsive" width={284} height={284} placeholder="blur" />
+                <Image
+                  alt="logo"
+                  src={Logo}
+                  layout="responsive"
+                  width={284}
+                  height={284}
+                  placeholder="blur"
+                />
               </div>
-              <div>
-                  <nav>
-                      <ul className=" font-bold text-casa-blue text-xl flex gap-x-8 ">
-                          <li>  <Link href="/about"><a>About</a></Link> </li>
-                          <li>  <Link href="/news"><a>News & Events</a></Link> </li>
-                          <li>  <Link href="/town-council"><a>Town Council</a></Link> </li>
-                          <li>  <Link href="/departments"><a>Departments</a></Link> </li>
-                          <li>  <Link href="/senior-center"><a>Senior Center</a></Link> </li>
-                          <li>  <Link href="/contact"><a>Contact</a></Link> </li>
-                      </ul>
-                  </nav>
-              </div>
-          </div></Container>
-          
-    </header>
-  )
-}
+            </a>
+          </Link>
 
-export default Nav
+          <div>
+            <nav>
+              <ul className=" font-bold text-casa-blue text-xl flex gap-x-8 ">
+                <li>
+                  {" "}
+                  <Link href="/about">
+                    <a>About</a>
+                  </Link>{" "}
+                </li>
+                <li>
+                  {" "}
+                  <Link href="/news">
+                    <a>News & Events</a>
+                  </Link>{" "}
+                </li>
+                <li>
+                  {" "}
+                  <Link href="/town-council">
+                    <a>Town Council</a>
+                  </Link>{" "}
+                </li>
+                <li>
+                  {" "}
+                  <Link href="/departments">
+                    <a>Departments</a>
+                  </Link>{" "}
+                </li>
+                <li>
+                  {" "}
+                  <Link href="/senior-center">
+                    <a>Senior Center</a>
+                  </Link>{" "}
+                </li>
+                <li>
+                  {" "}
+                  <Link href="/contact">
+                    <a>Contact</a>
+                  </Link>{" "}
+                </li>
+              </ul>
+            </nav>
+          </div>
+        </div>
+      </Container>
+    </header>
+  );
+};
+
+export default Nav;
